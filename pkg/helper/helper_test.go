bb6b0e963f1bf11fbe1656d01cf9667e11ae8c0a
Daniel Gruber <mail@danielgruber.com>
2017-11-26 18:43:13 +0100

    helper tests

--- pkg/helper/helper_test.go
+++ pkg/helper/helper_test.go
@@ -6,7 +6,8 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
-	"github.com/dgruber/drmaa2os/pkg/simpletrackerfakes"
+	"github.com/dgruber/drmaa2interface"
+	"github.com/dgruber/drmaa2os/pkg/jobtracker/simpletrackerfakes"
 )
 
 var _ = Describe("Helper", func() {
@@ -27,15 +28,26 @@ var _ = Describe("Helper", func() {
 
 	Context("Create array job out with single job submissions", func() {
 
+		jt := drmaa2interface.JobTemplate{RemoteCommand: "test"}
+
 		It("AddArrayJobAsSingleJobs should work", func() {
 			fakeTracker := simpletrackerfakes.New("testsession")
-			id, err := AddArrayJobAsSingleJobs(fakeTracker, 10, 110, 2)
+			_, err := AddArrayJobAsSingleJobs(jt, fakeTracker, 11, 110, 2)
 			Ω(err).Should(BeNil())
-			jobs, errJobs := fakeTracker.ListJobs(id)
+			jobs, errJobs := fakeTracker.ListJobs()
 			Ω(errJobs).Should(BeNil())
 			Ω(len(jobs)).Should(BeNumerically("==", 50))
 		})
 
+	})
+
+	Context("Error cases", func() {
+
+		It("should return nothing when array job id is not parsable", func() {
+			ajid, err := ArrayJobID2GUIDs("_")
+			Ω(err).ShouldNot(BeNil())
+			Ω(ajid).Should(BeEmpty())
+		})
 	})
 
 })
