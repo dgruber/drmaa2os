cf169f2007075184e3698a6b76ebfddb2b173d44
Daniel Gruber <daniel.gruber@theubercloud.com>
2021-09-26 21:01:30 +0200

    JobSession supports contact string

--- jobsession.go
+++ jobsession.go
@@ -1,6 +1,8 @@
 package drmaa2os
 
 import (
+	"fmt"
+	"log"
 	"time"
 
 	"github.com/dgruber/drmaa2interface"
@@ -14,6 +16,9 @@ import (
 type JobSession struct {
 	name    string
 	tracker []jobtracker.JobTracker
+	// libdrmaa stores newly created internal job session name
+	// inside contact string
+	contact string
 }
 
 // Close MUST perform the necessary action to disengage from the DRM system.
@@ -39,7 +44,12 @@ func (js *JobSession) Close() error {
 // value was originally provided, the default contact string from the
 // implementation MUST be returned.
 func (js *JobSession) GetContact() (string, error) {
-	return "", nil
+	if len(js.tracker) >= 1 {
+		if cs, ok := js.tracker[0].(jobtracker.ContactStringer); ok {
+			return cs.Contact()
+		}
+	}
+	return "not implemented", nil
 }
 
 // GetSessionName reports the session name, a value that resulted from the
@@ -82,10 +92,12 @@ func (js *JobSession) GetJobs(filter drmaa2interface.JobInfo) ([]drmaa2interface
 	for _, tracker := range js.tracker {
 		jobs, err := tracker.ListJobs()
 		if err != nil {
+			fmt.Printf("error listings jobs: %v", err)
 			return nil, err
 		}
 		for _, jobid := range jobs {
 			if jinfo, err := tracker.JobInfo(jobid); err != nil {
+				fmt.Printf("failed getting job info for job %s: %v", jobid, err)
 				continue
 			} else {
 				if d2hlp.JobInfoMatches(jinfo, filter) {
